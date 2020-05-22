762ea92122fd7ec5d50d80b26243775a82011ea4
Markus Petters <mdpetter@ncsu.edu>
2020-05-21 22:05:17 -0400

    remove lib-usb files

--- src/LabjackU6Library.jl
+++ src/LabjackU6Library.jl
@@ -11,15 +11,15 @@ module LabjackU6Library
 #-
 
 export
-   openUSBConnection,          # Opens a USB connection
-   closeUSBConnection,         # Closes a USB connection
+   openUSBConnection,             # Opens a USB connection
+   closeUSBConnection,            # Closes a USB connection
    getCalibrationInformation,     # Returns u6CalibrationInfo
    getTdacCalibrationInformation, # Returns u6CalibrationInfo LJTdac
-   setLJTDAC,                  # low-level call to set LJTDAC V
-   extendedChecksum!,          # Computes checksum
-   labjackSend,                # Send buffer to Labjack
-   labjackRead!,               # Read buffer from Labjack
-   calibrateAIN                # Calibrates AIN signal
+   setLJTDAC,                     # low-level call to set LJTDAC V
+   extendedChecksum!,             # Computes checksum
+   labjackSend,                   # Send buffer to Labjack
+   labjackRead!,                  # Read buffer from Labjack
+   calibrateAIN                   # Calibrates AIN signal
 
 export
    u6CalibrationInfo,
@@ -41,18 +41,32 @@ mutable struct u6CalibrationInfo
    ccConstants::NTuple{40,Float64}
 end
 
-# Data Structure of LJTDAC calibration constants
+"""
+   u6TDACCalibrationInfo
+
+Data Structure of LJTDAC calibration constants
+ - prodID: productID
+ - ccConstants: calibration constants
+
+"""
 mutable struct u6TDACCalibrationInfo
 	prodID::UInt8
 	ccConstants::NTuple{4,Float64}
 end
 
-# Generic Buffer structure to send and receive data via Hardware calls
+"""
+   u6TDACCalibrationInfo
+
+Generic Buffer structure to send and receive data via Hardware calls
+NTuple{N,UInt8}
+
+"""
 mutable struct labjackBuffer{N}
    buff::NTuple{N,UInt8}
 end
 
-const lib = dirname(Base.find_package("LabjackU6Library"))*"/libU6.so"
+#const lib = dirname(Base.find_package("LabjackU6Library"))*"/libU6.so"
+const lib = pwd()*"/libU6.so"
 include("u6ccalls.jl")
 
 end
